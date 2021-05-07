bfb31d2d65465a9abdaa7786e358ff2c6fb75dff
Durgesh Chandra Pandey -X (dupandey - ZENSAR TECHNOLOGIES INC at Cisco) <dupandey@cisco.com>
2021-05-07 20:59:12 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+import hospitalRecords from './share/records.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'people-help';
+
+  public hosRecords = hospitalRecords;
+  public closeResult: string | undefined;
+  teamDetail: any = [];
+
+  constructor(private modalService: NgbModal) { }
+  // {
+  //   hName: string,
+  //   icuAvail: number,
+  //   bedWithOxygenAvail: boolean,
+  //   ventilatorsAvail: number,
+  //   bipapAvail: number,
+  //   hfnoAvail: number,
+  //   teamDetail: string
+  // } 
+
+  open(content: any, data: any) {
+    this.modalService.open(content, {
+      centered: true,
+      backdrop: 'static'
+    });
+    this.teamDetail = data;
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
